739d394e34e0bd708766a04b6e9463a4510ab503
k1kimosha <40123755+k1kimosha@users.noreply.github.com>
2022-06-21 14:26:16 +0300

    Add [mute, init_report] command Add localization system

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const { Client, Intents } = require('discord.js');
-const { createPool } = require('mysql2');
-const config = require('./config.json');
+const { Client, Intents } = require("discord.js");
+const { createPool } = require("mysql2");
+const config = require("./config.json");
+
+var lang = {
+    "en": require("./localization/en.json")
+};
 
 const bot = new Client({
     intents: [
@@ -14,7 +18,7 @@ const bot = new Client({
 });
 
 const pool = createPool({
-    host:config.mysql.host,
+    host: config.mysql.host,
     port: config.mysql.port,
     user: config.mysql.user,
     password: config.mysql.password,
@@ -23,6 +27,83 @@ const pool = createPool({
 
 bot.login(config.token);
 
-bot.on('ready', () => {
-    bot.user.setPresence({status:'dnd'});
+bot.on("ready", () => {
+    bot.user.setPresence({ status: "dnd" });
+
+    bot.application.commands.set([
+        {
+            name: "init_report",
+            description: lang[config.lang].cmds.description.initReport,
+            type: "CHAT_INPUT",
+            defaultPermission: false
+        },
+        {
+            name: "mute",
+            description: lang[config.lang].cmds.description.mute.cmd,
+            type: "CHAT_INPUT",
+            defaultPermission: false,
+            options: [
+                {
+                    name: "user",
+                    type: "USER",
+                    description: lang[config.lang].cmds.description.mute.user,
+                    required: true
+                },
+                {
+                    name: "time",
+                    type: "NUMBER",
+                    description: lang[config.lang].cmds.description.mute.time,
+                    required: true
+                },
+                {
+                    name: "reason",
+                    type: "STRING",
+                    description: lang[config.lang].cmds.description.mute.reason,
+                    required: true
+                }
+            ]
+        }
+    ])
 });
+
+bot.on('interactionCreate', interact => {
+    if (interact.isCommand()) {
+        switch (interact.commandName) {
+            case "init_report": {
+                interact.reply({
+                    content: lang[config.lang].interact.initReport.text,
+                    components: [
+                        {
+                            type: "ACTION_ROW",
+                            components: [
+                                {
+                                    type: "BUTTON",
+                                    label: lang[config.lang].interact.initReport.button,
+                                    style: "SECONDARY",
+                                    customId: "reportCreate",
+                                    emoji: '💢'
+                                }
+                            ],
+                        }
+                    ]
+                })
+            }
+            case "mute": {
+                let target = interact.options.getMember("user");
+                let time = interact.options.getNumber("time");
+                let reason = interact.options.getString("reason");
+                if (target.moderatable) {
+                    target.timeout(time*60*1000, reason);
+                    interact.reply({
+                        content: lang[config.lang].interact.mute.replace("${target}", target.displayName).replace("${time}", time).replace("${reason}", reason)
+                    });
+                } else {
+                    interact.reply({
+                        content: lang[config.lang].err_not_permission,
+                        ephemeral: true
+                    });
+                }
+            }
+        }
+    }
+})
